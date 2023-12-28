c30f3e818defb424df5d884d72177e6b1fe7dfce
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-12-28 12:04:25 +0900

    add backup

---
+++ backup/backup.ts
@@ -0,0 +1,24 @@
+// BACKUP 復元用
+
+const targetFile = "";
+
+if (!Deno.statSync(targetFile).isFile) {
+    console.error(`"${targetFile}" は存在しません。`);
+}else {
+    const kv = await Deno.openKv();
+
+    const backup = await Deno.readTextFile(targetFile);
+
+    const json: {
+        key: string[];
+        value: any;
+    } = JSON.parse(backup);
+
+    for (const entry of json) {
+        await kv.set(entry.key, entry.value);
+    }
+
+    await kv.close();
+
+    console.log(`"${targetFile}" を復元しました。`);
+}
